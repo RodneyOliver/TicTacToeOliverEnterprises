0d2d09c3ab1570aac74681cafba2ae2430dd07c5
RodneyOliver <rodneywoliver@gmail.com>
2015-11-15 13:34:00 -0600

    Visual Updates
    Adding Visual Updates

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -9,12 +9,12 @@ public class GameDriver
 	public static void main(String[] args) 
 	{
 		JFrame mainGameFrame = new JFrame("TIC TAC TOE");
+		//JFrame.setDefaultLookAndFeelDecorated(true);
+		
 		mainGameFrame.setResizable(true);
-		//
 		mainGameFrame.setSize(1400,800);
-
 		mainGameFrame.setVisible(true);
-		mainGameFrame.setDefaultLookAndFeelDecorated(true);
+
 		mainGameFrame.setLocationRelativeTo(null);
 		mainGameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
