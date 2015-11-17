1fcd691fe087d5c7da36dd2e6fec0d0166467123
RodneyOliver <rodneywoliver@gmail.com>
2015-11-16 18:46:24 -0600

    Fixing Issues

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,7 +1,6 @@
-import java.awt.Color;
 import javax.swing.JFrame;
-import java.awt.SplashScreen;
 import javax.swing.WindowConstants;
+
 
 public class GameDriver 
 {
@@ -9,49 +8,15 @@ public class GameDriver
 	
 	public static void main(String[] args) 
 	{
-	
-		//EXAMPLE OF HOW USING TUTORIAL SPLASH SCREEN
-		/*		try
-		{
-		//JSplash splash = new JSplash(url, progress, messages, percent, versionString, versionStringFont, VersionNumColor, progressBarColor);
-		JSplash splash = new JSplash(GameDriver.class.getResource("splash.png"),true, true, false, "Version 0.1", null, Color.RED, Color.BLACK);
-		splash.splashOn();
-		// Call Method
-		splash.setProgress(20,"Init");
-		Thread.sleep(1000);
-		splash.setProgress(40,"Loading");
-		Thread.sleep(1000);
-		splash.setProgress(60,"Applying Configs");
-		Thread.sleep(1000);
-		splash.setProgress(80,"Starting App");
-		Thread.sleep(1000);
-		//
-		splash.splashOff();
-		
-		} catch (Exception e) 
-			{
-				e.printStackTrace();
-			}*/
-		
-		
-		/*
 		JFrame mainGameFrame = new JFrame("TIC TAC TOE");
-<<<<<<< HEAD
 		//JFrame.setDefaultLookAndFeelDecorated(true);
 		
 		mainGameFrame.setResizable(true);
 		mainGameFrame.setSize(1400,800);
 		mainGameFrame.setVisible(true);
 
-=======
-		//mainGameFrame.setDefaultLookAndFeelDecorated(true);
-		mainGameFrame.setResizable(true);
-		mainGameFrame.setSize(1400,800);
-		mainGameFrame.setVisible(true);
->>>>>>> origin/master
 		mainGameFrame.setLocationRelativeTo(null);
 		mainGameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		*/
 	}
 
 }
