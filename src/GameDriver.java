f22a27323a20a63a7b0074771581dfef96220c97
RodneyOliver <rodneywoliver@gmail.com>
2015-11-16 18:16:56 -0600

    Working with Splash

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,6 +1,7 @@
+import java.awt.Color;
 import javax.swing.JFrame;
+import java.awt.SplashScreen;
 import javax.swing.WindowConstants;
-
 
 public class GameDriver 
 {
@@ -8,6 +9,32 @@ public class GameDriver
 	
 	public static void main(String[] args) 
 	{
+	
+		//EXAMPLE OF HOW USING TUTORIAL SPLASH SCREEN
+		/*		try
+		{
+		//JSplash splash = new JSplash(url, progress, messages, percent, versionString, versionStringFont, VersionNumColor, progressBarColor);
+		JSplash splash = new JSplash(GameDriver.class.getResource("splash.png"),true, true, false, "Version 0.1", null, Color.RED, Color.BLACK);
+		splash.splashOn();
+		// Call Method
+		splash.setProgress(20,"Init");
+		Thread.sleep(1000);
+		splash.setProgress(40,"Loading");
+		Thread.sleep(1000);
+		splash.setProgress(60,"Applying Configs");
+		Thread.sleep(1000);
+		splash.setProgress(80,"Starting App");
+		Thread.sleep(1000);
+		//
+		splash.splashOff();
+		
+		} catch (Exception e) 
+			{
+				e.printStackTrace();
+			}*/
+		
+		
+		/*
 		JFrame mainGameFrame = new JFrame("TIC TAC TOE");
 		//JFrame.setDefaultLookAndFeelDecorated(true);
 		
@@ -17,6 +44,7 @@ public class GameDriver
 
 		mainGameFrame.setLocationRelativeTo(null);
 		mainGameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		*/
 	}
 
 }
