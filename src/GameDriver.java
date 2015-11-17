5325278a3385d3b32fdcfa8768cadba129b82310
RodneyOliver <rodneywoliver@gmail.com>
2015-11-17 06:53:11 -0600

    Working with Splash Screen

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,23 +1,38 @@
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
-
+import java.awt.SplashScreen;
+import java.awt.*;
 
 public class GameDriver 
 {
-	
-	
+
 	public static void main(String[] args) 
-	{
+	{		
+	    
+        final SplashScreen splash = SplashScreen.getSplashScreen();
+        if (splash == null) {
+            System.out.println("SplashScreen.getSplashScreen() returned null");
+            return;
+        }
+        Graphics2D g = splash.createGraphics();
+        if (g == null) {
+            System.out.println("g is null");
+            return;
+        }
+
+		
+		
+		/*
 		JFrame mainGameFrame = new JFrame("TIC TAC TOE");
 		//JFrame.setDefaultLookAndFeelDecorated(true);
-		
 		mainGameFrame.setResizable(true);
 		mainGameFrame.setSize(1400,800);
 		mainGameFrame.setVisible(true);
-
 		mainGameFrame.setLocationRelativeTo(null);
 		mainGameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		*/
 	}
+
 
 }
 
